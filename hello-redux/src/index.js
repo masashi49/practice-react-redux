4c8b8f5ad4b51d9c895a0089f4cf1e7c73040a71
yamada <yamada@reas.jp>
2019-12-02 14:13:54 +0900

    ディレクトリ分割、各reducerをconbineReducersで呼び出し

--- hello-redux/src/index.js
+++ hello-redux/src/index.js
@@ -5,54 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import {createStore} from 'redux';
+import allReducers from './reducers'
 
-
-//store
-
-//action
-const increment = () => {
-  return {
-    type: 'INCREMENT',
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT',
-  }
-}
-
-//reducre
-
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-  }
-};
-
-let store = createStore(counter);
-
-
-store.subscribe(() => console.log(store.getState()));
-
-//dispath
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-
-store.dispatch(decrement());
-store.dispatch(decrement());
-store.dispatch(decrement());
-store.dispatch(decrement());
+let store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+console.log(store)
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
