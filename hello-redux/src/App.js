cf76631025176fc911996c967f326c745f32aca1
yamada <yamada@reas.jp>
2019-12-03 09:54:28 +0900

    combineReducersにてcounterReducer与えていたkey名と、App.jsでstate.keyで呼び出す名前を一致させてなくて一瞬ハマった。

--- hello-redux/src/App.js
+++ hello-redux/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import {useSelector} from 'react-redux';
 
 function App() {
+    const count = useSelector(state => state.count);
+    const isLogged = useSelector(state => state.isLogged);
+
     return (
         <div className="App">
-            test
+            test {count}
+            {isLogged ? 'login!!!' : 'no-member'}
         </div>
     );
 }
